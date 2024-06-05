d4d2d242326278cbd86e94698619dfb5044dff04
Paolo Aimar <s297424@studenti.polito.it>
2024-06-05 12:20:06 +0200

    add files

--- UI/controller.py
+++ UI/controller.py
@@ -7,40 +7,22 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+
         self._listYear = []
-        self._listShape = []
+        self._listCountry = []
 
     def fillDD(self):
-        year = self._model.getYear()
-        for y in year:
-            self._view.ddyear.options.append(ft.dropdown.Option(f"{y}"))
+        pass
 
-    def fillShape(self, e):
-        year = self._view.ddyear.value
-        forme = self._model.getShape(int(year))
-        for f in forme:
-            self._view.ddshape.options.append(ft.dropdown.Option(f"{f}"))
-        self._view.update_page()
 
     def handle_graph(self, e):
-        self._view.txt_result.controls.clear()
-        year = self._view.ddyear.value
-        shape = self._view.ddshape.value
-        self._model.buildGraph(year, shape)
-        nNodes, nEdges = self._model.getCaratteristicheGrafo()
-        self._view.txt_result.controls.append(ft.Text("Grafo creato correttamente"))
-        self._view.txt_result.controls.append(ft.Text(f"Il grafo creato ha {nNodes} nodi e {nEdges} archi"))
-        dizionario = self._model.getPesoAdiacenti()
-        for key, value in dizionario.items():
-            self._view.txt_result.controls.append(ft.Text(f"Nodo {key} somma pesi su archi={value}"))
-        self._view.update_page()
+        pass
+
+
+
+    def handle_volume(self, e):
+        pass
+
 
     def handle_path(self, e):
-        self._view.txtOut2.controls.clear()
-        year = self._view.ddyear.value
-        shape = self._view.ddshape.value
-        cammino_migliore, peso_tot = self._model.getBestPath()
-        self._view.txtOut2.controls.append(ft.Text(f"il cammino massimo ha peso {peso_tot}"))
-        for o in range(len(cammino_migliore)-1):
-           self._view.txtOut2.controls.append(ft.Text(f"{cammino_migliore[o]}--{cammino_migliore[o+1]}"))
-        self._view.update_page()
+        pass
--- UI/view.py
+++ UI/view.py
@@ -13,49 +13,63 @@ class View(ft.UserControl):
         self._controller = None
         # graphical elements
         self._title = None
-        self.ddyear = None
-        self.ddshape = None
-        self.btn_graph = None
-        self.txt_result = None
-        self.txt_container = None
 
+        self.ddyear = None
+        self.ddcountry = None
         self.txtN = None
-        self.txtOut2 = None
+
+        self.btn_graph = None
+        self.btn_volume = None
         self.btn_path = None
 
+        self.txt_result = None
+        self.txtOut2 = None
+        self.txtOut3 = None
+
+        self.txt_container = None
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Lab13 - Ufo sighting", color="blue", size=24)
+        self._title = ft.Text("TdP 2024 - Lab12: Prova tema d'esame", color="blue", size=24)
         self._page.controls.append(self._title)
 
         #ROW with some controls
-        self.ddyear = ft.Dropdown(label="Anno", on_change=self._controller.fillShape)
-        self.ddshape = ft.Dropdown(label="Shape")
+        self.ddyear = ft.Dropdown(label="Anno")
+        self.ddcountry= ft.Dropdown(label="Nazione")
 
-
-        # button for the "creat graph" reply
         self.btn_graph = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handle_graph)
-        row1 = ft.Row([self.ddyear,self.ddshape, self.btn_graph],
+
+        row1 = ft.Row([self.ddyear, self.ddcountry, self.btn_graph],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
-
         self._controller.fillDD()
 
         # List View where the reply is printed
-        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self.txt_result = ft.ListView(expand=0, spacing=5, padding=5, auto_scroll=True)
         self._page.controls.append(self.txt_result)
         self._page.update()
 
-        self.btn_path = ft.ElevatedButton(text="Calcola percorso", on_click=self._controller.handle_path)
 
-        row2 = ft.Row([self.btn_path],
+        self.btn_volume = ft.ElevatedButton(text="Calcola Volumi", on_click=self._controller.handle_volume)
+        row2 = ft.Row([self.btn_volume],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row2)
 
         self.txtOut2 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
         self._page.controls.append(self.txtOut2)
         self._page.update()
+
+        self.txtN = ft.TextField(label="Lunghezza percorso")
+        self.btn_path = ft.ElevatedButton(text="Calcola percorso", on_click=self._controller.handle_path)
+
+        row3 = ft.Row([self.txtN, self.btn_path],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row3)
+
+        self.txtOut3 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self._page.controls.append(self.txtOut3)
+        self._page.update()
+
     @property
     def controller(self):
         return self._controller
